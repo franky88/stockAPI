ff941eb40cff40e79632f79643d88fd174e44589
Franklin P. Ramos <franky88@users.noreply.github.com>
2023-12-05 10:58:00 +0800

    updates

--- store/models.py
+++ store/models.py
@@ -19,6 +19,9 @@ class TimeStampedModel(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=120)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
